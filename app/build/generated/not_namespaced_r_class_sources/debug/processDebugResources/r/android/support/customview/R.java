eb381ea05cd635955e0e8e7aa78593edc555fee2
Luqman <mrice_87@yahoo.com>
2019-07-17 21:47:16 +0800

    ubah padding kat textview welcome sebelah kiri +8

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/customview/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/customview/R.java
@@ -104,8 +104,8 @@ public final class R {
         public static final int tag_unhandled_key_listeners = 0x7f070082;
         public static final int text = 0x7f070083;
         public static final int text2 = 0x7f070084;
-        public static final int time = 0x7f070087;
-        public static final int title = 0x7f070088;
+        public static final int time = 0x7f070089;
+        public static final int title = 0x7f07008a;
     }
     public static final class integer {
         private integer() {}

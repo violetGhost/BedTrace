688bccbfc57c1d2bf9873a2cca121c5edf829e5c
fatin.nabilah <fatin.nabilah@mindwave.my>
2019-07-18 23:09:58 +0800

    update design register bed and user profile

---
+++ app/src/main/java/com/example/bedtrace/register_beds.java
@@ -0,0 +1,13 @@
+package com.example.bedtrace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register_beds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_beds);
+    }
+}

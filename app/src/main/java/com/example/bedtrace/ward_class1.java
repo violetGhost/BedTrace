13b0905628aa5cda0221d21fbebe306fccb999fe
fatin.nabilah <fatin.nabilah@mindwave.my>
2019-07-21 22:20:49 +0800

    add screen main page and wardsss

---
+++ app/src/main/java/com/example/bedtrace/ward_class1.java
@@ -0,0 +1,13 @@
+package com.example.bedtrace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ward_class1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ward_class1);
+    }
+}
---
+++ app/src/main/java/com/example/bedtrace/ward_class3.java
@@ -0,0 +1,13 @@
+package com.example.bedtrace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ward_class3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ward_class3);
+    }
+}

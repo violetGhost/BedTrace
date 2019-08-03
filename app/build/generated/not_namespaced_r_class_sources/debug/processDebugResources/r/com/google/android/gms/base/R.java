edfb41053ea82b5c62ed4d3d1e672d7dacea2099
fatin.nabilah <fatin.nabilah@mindwave.my>
2019-08-03 11:30:38 +0800

    update UI

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -65,12 +65,12 @@ public final class R {
         public static final int adjust_height = 0x7f0a001b;
         public static final int adjust_width = 0x7f0a001c;
         public static final int auto = 0x7f0a0022;
-        public static final int dark = 0x7f0a003c;
-        public static final int icon_only = 0x7f0a0060;
-        public static final int light = 0x7f0a0080;
-        public static final int none = 0x7f0a0094;
-        public static final int standard = 0x7f0a00d7;
-        public static final int wide = 0x7f0a0131;
+        public static final int dark = 0x7f0a003d;
+        public static final int icon_only = 0x7f0a0061;
+        public static final int light = 0x7f0a0081;
+        public static final int none = 0x7f0a0095;
+        public static final int standard = 0x7f0a00d8;
+        public static final int wide = 0x7f0a0132;
     }
     public static final class string {
         private string() {}
